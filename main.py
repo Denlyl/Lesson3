aa08f70a7328a1917f82f9956755e7c86ee56075
Denlyl <deny1skz@gmail.com>
2024-09-05 14:57:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+homework_python = 12
+time_python = 1.5
+name_python = 'Python'
+time_for_one_ex = (time_python/homework_python)
+print('Курс',name_python,'Всего задач',homework_python,'Затрачено часов',time_python,'Среднее время выполнения',time_for_one_ex)
